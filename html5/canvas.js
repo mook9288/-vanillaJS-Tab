bfdc11ebc06df0545da830271c3f0ac09b6c29af
mook9288 <mook9288@gmail.com>
2020-06-23 15:47:48 +0900

    update. [src는 다 뿌려지는데.. 배열에 담기는건가....]

--- html5/canvas.js
+++ html5/canvas.js
@@ -1,22 +1,59 @@
-(() => {
+(() => {  
+
+  const thisImgJPGSrcArray = [];
+  
+  let imgIdx = 0;
+  let imgArrIdx = 0;
+  const canvas = document.querySelector('.canvas-img');
+  const ctx = canvas.getContext('2d');
   const imageWidth = 1440;
   const imageHeight = 810;
   const kvHeightPer = imageHeight / imageWidth;
-  
-  const canvas = document.querySelector('.canvas-img');
-  const ctx = canvas.getContext('2d');
-  const arrImage = [];
-  
+
   const canvasWidth = canvas.offsetWidth;
   const canvasHeight = (canvas.offsetWidth * kvHeightPer).toFixed();
-  
+
   document.querySelector('.canvas-img').setAttribute('width', canvasWidth);
   document.querySelector('.canvas-img').setAttribute('height', canvasHeight);
 
-  const img = new Image();
-  img.src = './images/test-1.jpg';
-  img.onload = function () {
-    ctx.drawImage(img, 0, 0, canvasWidth, canvasHeight);
-  };
+  const load = function () {
+    
+    const forLoad = function () {
+      const currentImgJPG = new Image();
+      const frameNum = 65;
+      const frameNumArrSize = frameNum - 1;
+      const imageFileName = './images/test-numbering0.jpg';
+      
+      // const fileNumber = imageFileName.replace(/[^-]/g, "");
+      const fileSrc = imageFileName.split("numbering0")[0];
+      const fileType = imageFileName.split("numbering0")[1];
+      
+      // for (let frameIdx = 1; frameIdx <= frameNum; frameIdx++) {
+      //   console.log(frameNum[frameIdx]);
+      //   currentImgJPG.src = fileSrc + frameNum[frameIdx] + fileType;
+      //   thisImgJPGSrcArray.push(currentImgJPG.src[1]);
+      // }
 
+      [...Array(frameNum)].forEach(() => {
+        imgIdx++;
+        currentImgJPG.src = fileSrc + imgIdx + fileType;
+        thisImgJPGSrcArray.push(currentImgJPG);
+        // console.log(currentImgJPG.src);
+      });
+      console.log(thisImgJPGSrcArray[0]);
+
+      [...Array(frameNumArrSize)].forEach(() => {
+        imgArrIdx++;
+        thisImgJPGSrcArray[imgArrIdx].onload = function () {
+          ctx.drawImage(thisImgJPGSrcArray[0], 0, 0, canvasWidth, canvasHeight);
+        }
+      });
+
+      // thisImgJPGSrcArray[4].onload = function () {
+      //   ctx.drawImage(thisImgJPGSrcArray[4], 0, 0, canvasWidth, canvasHeight);
+      // };
+    }
+		forLoad();
+  }
+  load();
 })();
