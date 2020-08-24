f432416dc78376f5223fcaeeda42bb1dcd3ac597
mook9288 <mook9288@gmail.com>
2020-08-24 15:14:51 +0900

    update. carousel

--- vanillaJS/carousel/carousel.js
+++ vanillaJS/carousel/carousel.js
@@ -1,21 +1,37 @@
-// 좌측 화살표 클릭시 이전 이미지, 우측 화살표 클릭시 다음 이미지
-var carouselList = document.querySelectorAll(".carousel-list li");
-var carouselDot = document.querySelectorAll(".carousel-dots button");
-var carouselArrow = document.querySelectorAll(".carousel-arr button");
+var carouselList = document.querySelectorAll(".test .carousel-list li");
+var carouselDot = document.querySelectorAll(".test .carousel-dots button");
+var nowImg = 0;
+var totalImg = carouselList.length;
+var currentImg;
 
-// 페이지 로딩 시, 첫 번째 이미지 화면에 보이기
-for (var listIdx = 0; listIdx < carouselList.length; listIdx++) {
-  carouselList[0].classList.add("active");
+document.querySelector(".test .arr-next").addEventListener("click", function () {
+  currentImg = nowImg;
+  nowImg++;
+  if (nowImg === totalImg) {
+    nowImg = 0;
+  }
+  prevNext(currentImg, nowImg);
+});
+
+document.querySelector(".test .arr-prev").addEventListener("click", function () {
+  currentImg = nowImg;
+  nowImg--;
+  if (nowImg === -1) {
+    nowImg = totalImg - 1;
+  }
+  prevNext(currentImg, nowImg);
+});
+
+[].forEach.call(carouselDot, function (el, idxDot) {
+  el.addEventListener("click", function () {
+    prevNext(nowImg, idxDot);
+    nowImg = idxDot;
+  });
+});
+
+function prevNext(current, change) {
+  carouselList[current].classList.remove("active");
+  carouselDot[current].classList.remove("active");
+  carouselList[change].classList.add("active");
+  carouselDot[change].classList.add("active");
 }
-for (var dotIdx = 0; dotIdx < carouselDot.length; dotIdx++) {
-  carouselDot[0].classList.add("active");
-}
-
-// 좌측 화살표 클릭시 이전 이미지 보여주기
-// 우측 화살표 클릭시 다음 이미지 보여주기
-// document.querySelector(".arr-next").addEventListener("click", arrowBtn);
-// var listCurrent;
-
-// function arrowBtn() {
-//   carouselList.classList.contains("active").indexOf();
-// }
