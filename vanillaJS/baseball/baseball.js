121667e2d0abd6e6476a034eb3f0a10efbdcd9ea
mook9288 <mook9288@gmail.com>
2020-09-08 14:36:41 +0900

    modify. [baseball] 완료

--- vanillaJS/baseball/baseball.js
+++ vanillaJS/baseball/baseball.js
@@ -4,26 +4,17 @@ var count = 0;
 var strike = 0;
 var ball = 0;
 var randomArray = [];
-var gameButton = document.querySelector(".btn-start");
-var replayButton = document.querySelector(".game-start-button button");
-var sendButton = document.querySelector(".btn-send");
+
+var gameButton = document.querySelector(".game-start-button button");
 var input = document.querySelector(".input-number");
 var scoreTable = document.querySelector(".score tbody");
 
-function enterNumberOnly() {
-  if ((event.keyCode < 48) || (event.keyCode > 57)) {
-    alert("숫자만 입력 가능합니다.");
-    event.returnValue = false;
-  }
-}
-
+// Start & Replay 버튼 작동
 gameButton.addEventListener("click", function (e) {
   randomArray = [];
   count = 0;
   for (var randomIndex = 0; randomIndex < number; randomIndex++) {
     randomArray[randomIndex] = Math.floor(Math.random() * 10);
-
-    // 중복 숫자 처리
     for (var overlap = 0; overlap < randomIndex; overlap++) {
       if (randomArray[randomIndex] === randomArray[overlap]) {
         randomIndex--;
@@ -31,77 +22,70 @@ gameButton.addEventListener("click", function (e) {
       }
     }
   }
-  gameButton.setAttribute("class", "btn-replay");
   gameButton.innerHTML="Game Replay";
   input.value = '';
   input.removeAttribute("disabled");
-  sendButton.removeAttribute("disabled");
   scoreTable.innerHTML = '';
-  console.log(randomArray);
 });
 
-
-document.querySelector(".btn-send").addEventListener("click", function (e) {
-  var inputValue = input.value;
+// Input 입력값 제한 및 Enter키 눌렀을 때
+function enterKeyDown() {
   var valueArray = [];
-
   ball = 0;
   strike = 0;
 
-  if (input.value.length === number) {
-    count++;
+  if(event.keyCode == 13) {
+    if (input.value.length === number) {
+      count++;
 
-    for (var valueArrayIndex = 0; valueArrayIndex < input.value.length; valueArrayIndex++) {
-      valueArray.push(parseInt(input.value[valueArrayIndex]));
-    }
+      // 입력값 배열로 받기
+      for (var valueArrayIndex = 0; valueArrayIndex < input.value.length; valueArrayIndex++) {
+        valueArray[valueArrayIndex] = input.value[valueArrayIndex];
+      }
 
-    for (var i = 0; i < number; i++) {
-      for (var j = 0; j < number; j++) {
-        if (randomArray[i] == valueArray[j]) {
-          if (i === j) {
-            strike++;
-          } else {
-            ball++;
+      // 생성된 값과 입력한 값 대응 비교..
+      for (var i = 0; i < number; i++) {
+        for (var j = 0; j < number; j++) {
+          if (randomArray[i] == valueArray[j]) {
+            if (i === j) {
+              strike++;
+            } else {
+              ball++;
+            }
+            break;
           }
-          break;
         }
       }
+
+      scoreTable.innerHTML += `<tr><td>${count}</td><td>${valueArray}</td><td>${strike}S ${ball}B</td></tr>`;
+
+      // 3자리 모두 맞을 때
+      if (strike === 3) {
+        alert(count + "번째 시도만에 정답 " + randomArray);
+        reset(count);
+      }
+
+      // 아웃 카운트 처리
+      if (count >= 10) {
+        alert("Game Over \n정답은 " + randomArray);
+        reset(count);
+      }
+    } else {
+      alert("숫자 3자리만 입력하세요.");
     }
-
-    scoreTable.innerHTML += `<tr><td>${count}</td><td>${valueArray}</td><td>${strike}S ${ball}B</td></tr>`
-  } else {
-    alert("숫자 3자리만 입력하세요.");
     input.value = '';
+  } else if ((event.keyCode < 48) || (event.keyCode > 57)) {
+    alert("숫자만 입력 가능합니다.");
+    event.returnValue = false;
   }
+}
 
-  // 3자리 모두 맞을 때
-  if (strike === 3) {
-    reset(count);
-  }
-
-  // 아웃 카운트 처리
-  if (count >= 9) {
-    reset(count);
-  }
-
-  console.log(randomArray, valueArray, strike, ball);
-});
-
-
+// 중복되는 reset
 function reset(count) {
-  if (count >= 9) {
-    alert("Game Over \n정답은 " + randomArray);
-
-  } else {
-    alert(count + '번째 시도만에 정답');
-  }
   count = 0;
-  input.value='';
-  input.removeAttribute("disabled");
   randomArray = [];
-  gameButton.setAttribute("class", "btn-start");
   gameButton.innerHTML="Game Start!!!";
+  input.value='';
   input.setAttribute("disabled", true);
-  sendButton.setAttribute("disabled", true);
   scoreTable.innerHTML = '';
 }
