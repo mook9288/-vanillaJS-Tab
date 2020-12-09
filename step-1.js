8e890ce21d24f2cea3b5fe8e28fa3771f871add6
mook <mook9288@gmail.com>
2020-12-09 19:10:56 +0900

    Add push to the left

--- step-1.js
+++ step-1.js
@@ -20,10 +20,19 @@
 
     if (!inputDirection.value) {
       return alert("방향을 입력하세요.");
-    } else if (inputDirection.value.replace(/^[LRlr]$/, "")) {
+    } else if (inputDirection.value.replace(/[LRlr]/, "")) {
       return alert("방향을 나타내는 L, R 의 대소문자만 입력 가능합니다.\n왼쪽으로 밀어낼 경우, L 또는 l\n오른쪽으로 밀어낼 경우, R 또는 r");
     }
 
-    return resultText.textContent = inputWord.value;
+    const wordSplit = inputWord.value;
+    const movement = +inputNumber.value;
+    const directionLeft = inputDirection.value === "L" || inputDirection.value === "l";
+    let wordResult = "";
+
+    if (directionLeft) {
+      wordResult = wordSplit.slice(movement) + wordSplit.slice(0, movement);
+    }
+
+    return resultText.textContent = wordResult;
   }
 })();
