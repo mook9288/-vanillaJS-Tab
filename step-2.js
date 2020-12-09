3434f1282c9dc88cfce1cdfc6d2a9bf66984f1bf
mook <mook9288@gmail.com>
2020-12-10 06:06:37 +0900

    Add cube runs hourly

--- step-2.js
+++ step-2.js
@@ -23,7 +23,17 @@
     if (!inpCode.value) {
       return alert("CUBE 동작 코드를 입력하세요.");
     }
-
+    const CUBE_CODE = {
+      U: "U",
+      U_OPP: "U'",
+      R: "R",
+      R_OPP: "R'",
+      L: "L",
+      L_OPP: "L'",
+      B: "B",
+      B_OPP: "B'",
+      Q: "Q",
+    }
     const inpCodeArray = inpCode.value.toUpperCase().split('');
 
     inpCodeArray.forEach(function (item, index, arr) {
@@ -32,5 +42,30 @@
         arr.splice(index, 1);
       }
     });
+
+    for (let i = 0; i < inpCodeArray.length; i++) {
+      setTimeout(() => {
+        const element = inpCodeArray[i];
+        if (element === CUBE_CODE.U) {
+          console.log("U  가장 윗줄을 왼쪽으로 한 칸 밀기");
+        } else if (element === CUBE_CODE.U_OPP) {
+          console.log("U' 가장 윗줄을 오른쪽으로 한 칸 밀기");
+        } else if (element === CUBE_CODE.R) {
+          console.log("R  가장 오른쪽 줄을 위로 한 칸 밀기");
+        } else if (element === CUBE_CODE.R_OPP) {
+          console.log("R' 가장 오른쪽 줄을 아래로 한 칸 밀기");
+        } else if (element === CUBE_CODE.L) {
+          console.log("L  가장 왼쪽 줄을 아래로 한 칸 밀기");
+        } else if (element === CUBE_CODE.L_OPP) {
+          console.log("L' 가장 왼쪽 줄을 위로 한 칸 밀기");
+        } else if (element === CUBE_CODE.B) {
+          console.log("B  가장 아랫줄을 오른쪽으로 한 칸 밀기");
+        } else if (element === CUBE_CODE.B_OPP) {
+          console.log("B' 가장 아랫줄을 왼쪽으로 한 칸 밀기");
+        } else if (element === CUBE_CODE.Q) {
+          console.log("Q  Bye~를 출력하고 프로그램을 종료한다.");
+        }
+      }, 1000 * i);
+    }
   }
 })();
