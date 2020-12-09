2f9088db8d5881b6d0a0d89e1e931fc7699377c9
mook <mook9288@gmail.com>
2020-12-10 05:31:19 +0900

    Add cube code value to array

--- step-2.js
+++ step-2.js
@@ -1,8 +1,36 @@
 (function () {
   const initCubeArray = ["R", "R", "W", "G", "C", "W", "G", "B", "B"];
   const cube = document.querySelector(".cube");
+  const inpCode = document.querySelector(".inp-code");
+  const resultButton = document.querySelector(".btn-result");
 
   for (let i = 0; i < initCubeArray.length; i++) {
     cube.innerHTML += `<span>${initCubeArray[i]}</span>`;
+  }
+
+  resultButton.addEventListener("click", handleClickEvent);
+  // const
+  // > U  가장 윗줄을 왼쪽으로 한 칸 밀기 RRW -> RWR
+  // > U' 가장 윗줄을 오른쪽으로 한 칸 밀기 RRW -> WRR
+  // > R  가장 오른쪽 줄을 위로 한 칸 밀기 WWB -> WBW
+  // > R' 가장 오른쪽 줄을 아래로 한 칸 밀기 WWB -> BWW
+  // > L  가장 왼쪽 줄을 아래로 한 칸 밀기 RGG -> GRG (L의 경우 R과 방향이 반대임을 주의한다.)
+  // > L' 가장 왼쪽 줄을 위로 한 칸 밀기 RGG -> GGR
+  // > B  가장 아랫줄을 오른쪽으로 한 칸 밀기 GBB -> BGB (B의 경우도 U와 방향이 반대임을 주의한다.)
+  // > B' 가장 아랫줄을 왼쪽으로 한 칸 밀기 GBB -> BBG
+  // > Q  Bye~를 출력하고 프로그램을 종료한다.
+  function handleClickEvent() {
+    if (!inpCode.value) {
+      return alert("CUBE 동작 코드를 입력하세요.");
+    }
+
+    const inpCodeArray = inpCode.value.toUpperCase().split('');
+
+    inpCodeArray.forEach(function (item, index, arr) {
+      if (item === "'") {
+        arr[index - 1] += "'";
+        arr.splice(index, 1);
+      }
+    });
   }
 })();
