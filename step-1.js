0c3b95849d24b979a37859c8803ecd40a4e869b4
mook <mook9288@gmail.com>
2020-12-09 19:28:05 +0900

    Add push to the right

--- step-1.js
+++ step-1.js
@@ -15,13 +15,15 @@
     if (!inputNumber.value) {
       return alert("숫자를 입력하세요.");
     } else if (-100 > inputNumber.value || 100 <= inputNumber.value) {
-      return alert("-100 <= N < 100\n-100과 100 사이의 정수를 입력하세요.");
+      alert("-100 <= N < 100\n-100과 100 사이의 정수를 입력하세요.");
+      return inputNumber.value = '';
     }
 
     if (!inputDirection.value) {
       return alert("방향을 입력하세요.");
     } else if (inputDirection.value.replace(/[LRlr]/, "")) {
-      return alert("방향을 나타내는 L, R 의 대소문자만 입력 가능합니다.\n왼쪽으로 밀어낼 경우, L 또는 l\n오른쪽으로 밀어낼 경우, R 또는 r");
+      alert("방향을 나타내는 L, R 의 대소문자만 입력 가능합니다.\n왼쪽으로 밀어낼 경우, L 또는 l\n오른쪽으로 밀어낼 경우, R 또는 r");
+      return inputDirection.value = '';
     }
 
     const wordSplit = inputWord.value;
@@ -31,6 +33,8 @@
 
     if (directionLeft) {
       wordResult = wordSplit.slice(movement) + wordSplit.slice(0, movement);
+    } else {
+      wordResult = wordSplit.slice(-movement) + wordSplit.slice(0, -movement);
     }
 
     return resultText.textContent = wordResult;
