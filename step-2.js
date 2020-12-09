6ebb683996c7311fbe725e25f08c177641ecb050
mook <mook9288@gmail.com>
2020-12-10 00:25:00 +0900

    Update cube initial status

--- step-2.js
+++ step-2.js
@@ -1,3 +1,8 @@
 (function () {
+  const initCubeArray = ["R", "R", "W", "G", "C", "W", "G", "B", "B"];
+  const cube = document.querySelector(".cube");
 
+  for (let i = 0; i < initCubeArray.length; i++) {
+    cube.innerHTML += `<span>${initCubeArray[i]}</span>`;
+  }
 })();
