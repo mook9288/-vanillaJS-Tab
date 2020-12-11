9d7f48c57df74740bfd496acaa79b9f2f47480fa
mook <mook9288@gmail.com>
2020-12-11 17:11:00 +0900

    Add handleClickEvent() for input value check

--- step-3.js
+++ step-3.js
@@ -8,16 +8,31 @@
     ["R", "R", "R", "R", "R", "R", "R", "R", "R"],
   ];
   const cubes = document.querySelectorAll(".cube");
+  const inpCode = document.querySelector(".inp-code");
+  const resultButton = document.querySelector(".btn-result");
 
   fillCubeCell();
+  resultButton.addEventListener("click", handleClickEvent);
 
   function fillCubeCell() {
-    cubes.innerHTML = "";
+    [...cubes].innerHTML = "";
 
     for (let i = 0; i < initCubeArray.length; i++) {
       for (let j = 0; j < initCubeArray[i].length; j++) {
         cubes[i].innerHTML += `<span class="color-${i}">${initCubeArray[i][j]}</span>`;
       }
+    }
+  }
+
+  function handleClickEvent() {
+    if (!inpCode.value) {
+      return alert("CUBE 동작 코드를 입력하세요.");
+    } else if (inpCode.value[0] === "'") {
+      alert("맨 앞에 '은 올 수 없습니다.");
+      return inpCode.value = "";
+    } else if (inpCode.value.replace(/[udlrfb']/gi, "")) {
+      alert("올바른 CUBE 동작 코드가 아닙니다.");
+      return inpCode.value = "";
     }
   }
 })();
