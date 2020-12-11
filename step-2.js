3aba1afa26c2f561f3ed6125e6258a69ece9b53a
mook <mook9288@gmail.com>
2020-12-11 09:50:26 +0900

    Simplify setTimeout() inside handleClickEvent()

--- step-2.js
+++ step-2.js
@@ -4,44 +4,28 @@
   const inpCode = document.querySelector(".inp-code");
   const resultButton = document.querySelector(".btn-result");
 
-  const CUBE_CODE = {
-    U: "U",
-    U_OPP: "U'",
-    R: "R",
-    R_OPP: "R'",
-    L: "L",
-    L_OPP: "L'",
-    B: "B",
-    B_OPP: "B'",
-    Q: "Q",
-  }
-
   fillCubeCell();
   resultButton.addEventListener("click", handleClickEvent);
 
   function fillCubeCell() {
-    cube.innerHTML = '';
+    cube.innerHTML = "";
     for (let i = 0; i < initCubeArray.length; i++) {
       cube.innerHTML += `<span>${initCubeArray[i]}</span>`;
     }
   }
 
-  // const
-  // > U  가장 윗줄을 왼쪽으로 한 칸 밀기 RRW -> RWR
-  // > U' 가장 윗줄을 오른쪽으로 한 칸 밀기 RRW -> WRR
-  // > R  가장 오른쪽 줄을 위로 한 칸 밀기 WWB -> WBW
-  // > R' 가장 오른쪽 줄을 아래로 한 칸 밀기 WWB -> BWW
-  // > L  가장 왼쪽 줄을 아래로 한 칸 밀기 RGG -> GRG (L의 경우 R과 방향이 반대임을 주의한다.)
-  // > L' 가장 왼쪽 줄을 위로 한 칸 밀기 RGG -> GGR
-  // > B  가장 아랫줄을 오른쪽으로 한 칸 밀기 GBB -> BGB (B의 경우도 U와 방향이 반대임을 주의한다.)
-  // > B' 가장 아랫줄을 왼쪽으로 한 칸 밀기 GBB -> BBG
-  // > Q  Bye~를 출력하고 프로그램을 종료한다.
   function handleClickEvent() {
     if (!inpCode.value) {
       return alert("CUBE 동작 코드를 입력하세요.");
+    } else if (inpCode.value[0] === "'") {
+      alert("맨 앞에 '은 올 수 없습니다.");
+      return inpCode.value = '!!!';
+    } else if (inpCode.value.replace(/[urlbq']/gi, "")) {
+      alert("올바른 CUBE 동작 코드가 아닙니다.");
+      return inpCode.value = '???';
     }
 
-    const inpCodeArray = inpCode.value.toUpperCase().split('');
+    const inpCodeArray = inpCode.value.toUpperCase().split("");
 
     inpCodeArray.forEach(function (item, index, arr) {
       if (item === "'") {
@@ -51,140 +35,133 @@
     });
 
     for (let i = 0; i < inpCodeArray.length; i++) {
-      setTimeout(() => {
-        const element = inpCodeArray[i];
-        let movingLine = '';
-        let cellPush = '';
-        let horizontalLine = '';
-        let verticalityLine = [];
-
-        if (element === CUBE_CODE.U) {
-          console.log("U  가장 윗줄을 왼쪽으로 한 칸 밀기");
-          movingLine = meoveCube(CUBE_CODE.U);
-          horizontalLine = initCubeArray.slice(movingLine[0], 3);
-          cellPush = horizontalLine.shift();
-          horizontalLine.splice(movingLine[2], 0, cellPush);
-          initCubeArray.splice(movingLine[0], 3, horizontalLine);
-          initCubeArray = extendFlatArray(initCubeArray);
-        } else if (element === CUBE_CODE.U_OPP) {
-          console.log("U' 가장 윗줄을 오른쪽으로 한 칸 밀기");
-          movingLine = meoveCube(CUBE_CODE.U_OPP);
-          horizontalLine = initCubeArray.slice(movingLine[0], 3);
-          cellPush = horizontalLine.pop();
-          horizontalLine.unshift(cellPush);
-          initCubeArray.splice(movingLine[0], 3, horizontalLine);
-          initCubeArray = extendFlatArray(initCubeArray);
-        } else if (element === CUBE_CODE.B) {
-          console.log("B  가장 아랫줄을 오른쪽으로 한 칸 밀기");
-          movingLine = meoveCube(CUBE_CODE.B_OPP);
-          horizontalLine = initCubeArray.slice(movingLine[0]);
-          cellPush = horizontalLine.pop();
-          horizontalLine.unshift(cellPush);
-          initCubeArray.splice(movingLine[0], 3, horizontalLine);
-          initCubeArray = extendFlatArray(initCubeArray);
-        } else if (element === CUBE_CODE.B_OPP) {
-          console.log("B' 가장 아랫줄을 왼쪽으로 한 칸 밀기");
-          movingLine = meoveCube(CUBE_CODE.B_OPP);
-          horizontalLine = initCubeArray.slice(movingLine[0]);
-          cellPush = horizontalLine.shift();
-          horizontalLine.splice(movingLine[2], 0, cellPush);
-          initCubeArray.splice(movingLine[0], 3, horizontalLine);
-          initCubeArray = extendFlatArray(initCubeArray);
-        } else if (element === CUBE_CODE.R) {
-          console.log("R  가장 오른쪽 줄을 위로 한 칸 밀기");
-          movingLine = meoveCube(CUBE_CODE.R);
-
-          let count = 0;
-
-          for (let i = 0; i < movingLine.length; i++) {
-            verticalityLine[i] = initCubeArray[movingLine[i]];
-          }
-
-          cellPush = verticalityLine.shift();
-          verticalityLine.splice(movingLine[2], 0, cellPush);
-
-          for (let j = 0; j < initCubeArray.length; j++) {
-            const rowNum = j % 3;
-            if (rowNum === 2) {
-              initCubeArray[j] = verticalityLine[count];
-              count++;
-            }
-          }
-        } else if (element === CUBE_CODE.R_OPP) {
-          console.log("R' 가장 오른쪽 줄을 아래로 한 칸 밀기");
-          movingLine = meoveCube(CUBE_CODE.R_OPP);
-
-          let count = 0;
-
-          for (let i = 0; i < movingLine.length; i++) {
-            verticalityLine[i] = initCubeArray[movingLine[i]];
-          }
-
-          cellPush = verticalityLine.pop();
-          verticalityLine.unshift(cellPush);
-
-          for (let j = 0; j < initCubeArray.length; j++) {
-            const rowNum = j % 3;
-            if (rowNum === 2) {
-              initCubeArray[j] = verticalityLine[count];
-              count++;
-            }
-          }
-        } else if (element === CUBE_CODE.L) {
-          console.log("L  가장 왼쪽 줄을 아래로 한 칸 밀기");
-          movingLine = meoveCube(CUBE_CODE.L);
-
-          let count = 0;
-
-          for (let i = 0; i < movingLine.length; i++) {
-            verticalityLine[i] = initCubeArray[movingLine[i]];
-          }
-          console.log(verticalityLine);
-
-          cellPush = verticalityLine.pop();
-          verticalityLine.unshift(cellPush);
-
-          for (let j = 0; j < initCubeArray.length; j++) {
-            const rowNum = j % 3;
-            if (rowNum === 0) {
-              initCubeArray[j] = verticalityLine[count];
-              count++;
-            }
-          }
-        } else if (element === CUBE_CODE.L_OPP) {
-          console.log("L' 가장 왼쪽 줄을 위로 한 칸 밀기");
-          movingLine = meoveCube(CUBE_CODE.L_OPP);
-
-          let count = 0;
-
-          for (let i = 0; i < movingLine.length; i++) {
-            verticalityLine[i] = initCubeArray[movingLine[i]];
-          }
-          console.log(verticalityLine);
-
-          cellPush = verticalityLine.shift();
-          verticalityLine.splice(movingLine[2], 0, cellPush);
-
-          for (let j = 0; j < initCubeArray.length; j++) {
-            const rowNum = j % 3;
-            if (rowNum === 0) {
-              initCubeArray[j] = verticalityLine[count];
-              count++;
-            }
-          }
-        } else if (element === CUBE_CODE.Q) {
-          console.log("Q  Bye~를 출력하고 프로그램을 종료한다.");
-          alert("Bye~");
-          initCubeArray = ["R", "R", "W", "G", "C", "W", "G", "B", "B"];
-          inpCode.value = '';
-        }
-        console.log(initCubeArray);
-        return fillCubeCell();
-      }, 1000 * i);
+      const element = inpCodeArray[i];
+      setTimeout(moveCube, 1000 * i, element);
     }
   }
 
-  function meoveCube(line) {
+  function moveCube(line) {
+    let movingLine = moveLine(line);
+    let movingLineNum = movingLine.length;
+    let movingLineLastIndex = movingLineNum - 1;
+    let cellPush = "";
+    let horizontalLine = "";
+    let verticalityLine = [];
+    let count = 0;
+    const CUBE_CODE = {
+      U: "U",
+      U_OPP: "U'",
+      R: "R",
+      R_OPP: "R'",
+      L: "L",
+      L_OPP: "L'",
+      B: "B",
+      B_OPP: "B'",
+      Q: "Q",
+    }
+    const CUBE_DIR = {
+      HOR_T: 3,
+      HOR_B: 0,
+      VER_L: 0,
+      VER_R: 2,
+      FLAG_T: true,
+      FLAG_F: false,
+    }
+
+    switch (line) {
+      case CUBE_CODE.U:
+        moveDirLift(CUBE_DIR.HOR_T, CUBE_DIR.FLAG_T);
+        break;
+
+      case CUBE_CODE.U_OPP:
+        moveDirRight(CUBE_DIR.HOR_T, CUBE_DIR.FLAG_F);
+        break;
+
+      case CUBE_CODE.B:
+        moveDirRight(CUBE_DIR.HOR_B, CUBE_DIR.FLAG_T);
+        break;
+
+      case CUBE_CODE.B_OPP:
+        moveDirLift(CUBE_DIR.HOR_B, CUBE_DIR.FLAG_F);
+        break;
+
+      case CUBE_CODE.R:
+        moveVerticality(CUBE_DIR.VER_R, CUBE_DIR.FLAG_T);
+        break;
+
+      case CUBE_CODE.R_OPP:
+        moveVerticality(CUBE_DIR.VER_R, CUBE_DIR.FLAG_F);
+        break;
+
+      case CUBE_CODE.L:
+        moveVerticality(CUBE_DIR.VER_L, CUBE_DIR.FLAG_F);
+        break;
+
+      case CUBE_CODE.L_OPP:
+        moveVerticality(CUBE_DIR.VER_L, CUBE_DIR.FLAG_T);
+        break;
+
+      case CUBE_CODE.Q:
+        alert("Bye~");
+        initCubeArray = ["R", "R", "W", "G", "C", "W", "G", "B", "B"];
+        inpCode.value = "";
+        break;
+
+      default:
+        break;
+    }
+
+    function moveDirLift(num, dir) {
+      if (dir) {
+        horizontalLine = initCubeArray.slice(movingLine[0], num);
+      } else {
+        horizontalLine = initCubeArray.slice(movingLine[0]);
+      }
+
+      cellPush = horizontalLine.shift();
+      horizontalLine.splice(movingLine[movingLineLastIndex], 0, cellPush);
+      initCubeArray.splice(movingLine[0], movingLineNum, horizontalLine);
+      initCubeArray = extendFlatArray(initCubeArray);
+    }
+
+    function moveDirRight(num, dir) {
+      if (dir) {
+        horizontalLine = initCubeArray.slice(movingLine[0]);
+      } else {
+        horizontalLine = initCubeArray.slice(movingLine[0], num);
+      }
+
+      cellPush = horizontalLine.pop();
+      horizontalLine.unshift(cellPush);
+      initCubeArray.splice(movingLine[0], movingLineNum, horizontalLine);
+      initCubeArray = extendFlatArray(initCubeArray);
+    }
+
+    function moveVerticality(num, dir) {
+      for (let i = 0; i < movingLineNum; i++) {
+        verticalityLine[i] = initCubeArray[movingLine[i]];
+      }
+
+      if (dir) {
+        cellPush = verticalityLine.shift();
+        verticalityLine.splice(movingLine[movingLineLastIndex], 0, cellPush);
+      } else {
+        cellPush = verticalityLine.pop();
+        verticalityLine.unshift(cellPush);
+      }
+
+      for (let j = 0; j < initCubeArray.length; j++) {
+        if (j % movingLineNum === num) {
+          initCubeArray[j] = verticalityLine[count];
+          count++;
+        }
+      }
+    }
+
+    return fillCubeCell();
+  }
+
+  function moveLine(line) {
     const selectedLine = {
       "U": [0, 1, 2],
       "U'": [0, 1, 2],
