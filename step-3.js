7dbf578c81ed28e31105ee5a7f2d83626a6acc5d
mook <mook9288@gmail.com>
2020-12-13 15:32:50 +0900

    Add move left cube And Move position of a function

--- step-3.js
+++ step-3.js
@@ -15,6 +15,14 @@
     U_OPP: "U'",
     D: "D",
     D_OPP: "D'",
+    L: "L",
+    L_OPP: "L'",
+    R: "R",
+    R_OPP: "R'",
+    F: "F",
+    F_OPP: "F'",
+    B: "B",
+    B_OPP: "B'",
   }
   const CUBE_FACE = {
     UP: 0,
@@ -27,7 +35,13 @@
   const CUBE_DIR = {
     LR: "from L to R",
     RL: "from R to L",
+    L: "L",
+    R: "R",
   }
+  const divisionNum = 3;
+  // let faceArray = [];
+  let movingLineArray = [];
+  let movingCellArray = "";
 
   fillCubeCell();
   resultButton.addEventListener("click", handleClickEvent);
@@ -66,17 +80,42 @@
         arr.splice(index, 1);
       }
     });
-    console.log(inpCodeArray);
 
     for (let i = 0; i < inpCodeArray.length; i++) {
       setTimeout(categorizeCode, 1000 * i, inpCodeArray[i]);
     }
   }
 
-  const divisionNum = 3;
-  let faceArray = [];
-  let movingLineArray = [];
-  let movingCellArray = "";
+  function rotateLine(line) {
+    const selected = {
+      0: [1, 2, 3, 4],
+      1: [0, 2, 5, 4],
+      2: [0, 1, 3, 5],
+      3: [0, 2, 5, 4],
+      4: [0, 1, 2, 3],
+      5: [1, 2, 3, 4],
+    }
+
+    return selected[line];
+  }
+
+  function rotateSideCell(line) {
+    const selected = {
+      "L": [0, 3, 6],
+      "R": [2, 5, 8],
+    }
+
+    return selected[line];
+  }
+
+  function rotatePosition(line) {
+    const selected = {
+      "from L to R": [6, 3, 0, 7, 4, 1, 8, 5, 2],
+      "from R to L": [2, 5, 8, 1, 4, 7, 0, 3, 6],
+    }
+
+    return selected[line];
+  }
 
   function categorizeCode(code) {
     let movedLine = "";
@@ -89,7 +128,7 @@
 
       case CUBE_CODE.U_OPP:
         movedLine = rotateLine(CUBE_FACE.UP);
-        rotateTop(movedLine, CUBE_FACE.UP, CUBE_DIR.RL)
+        rotateTop(movedLine, CUBE_FACE.UP, CUBE_DIR.RL);
         break;
 
       case CUBE_CODE.D:
@@ -99,8 +138,24 @@
 
       case CUBE_CODE.D_OPP:
         movedLine = rotateLine(CUBE_FACE.DOWN);
-        rotateBottom(movedLine, CUBE_FACE.DOWN, CUBE_DIR.RL)
+        rotateBottom(movedLine, CUBE_FACE.DOWN, CUBE_DIR.RL);
         break;
+
+      case CUBE_CODE.L:
+        movedLine = rotateLine(CUBE_FACE.LEFT);
+        rotateLeft(movedLine, CUBE_FACE.LEFT, CUBE_DIR.LR, CUBE_DIR.L);
+        break;
+
+      case CUBE_CODE.L_OPP:
+        movedLine = rotateLine(CUBE_FACE.LEFT);
+        rotateLeft(movedLine, CUBE_FACE.LEFT, CUBE_DIR.RL, CUBE_DIR.L);
+        break;
+
+      case CUBE_CODE.F:
+        break;
+
+      case CUBE_CODE.F_OPP:
+      break;
 
       case CUBE_CODE.Q:
         alert("Bye~");
@@ -122,14 +177,42 @@
     return fillCubeCell();
   }
 
-  function rotateTop(line, face, dir) {
-    for (let i = 0; i < initCubeArray.length; i++) {
-      faceArray.push(initCubeArray[i]);
-    }
-
+  function selectMovingFace(line, face) {
     for (let i = 0; i < line.length; i++) {
-      movingLineArray.push(initCubeArray[line[i]].splice(0, divisionNum));
+      switch (face) {
+        case CUBE_FACE.UP:
+          movingLineArray.push(initCubeArray[line[i]].splice(0, divisionNum));
+          break;
+
+        case CUBE_FACE.DOWN:
+          movingLineArray.push(initCubeArray[line[i]].splice(divisionNum * 2));
+          break;
+
+        case CUBE_FACE.LEFT:
+          movingLineArray.push(initCubeArray[line[i]]);
+          break;
+
+        default:
+          break;
+      }
     }
+    return movingLineArray;
+  }
+
+  function rotateFaceOfCube(init, pos) {
+    const movedCycleArray = [];
+
+    for (let i = 0; i < init.length; i++) {
+      movedCycleArray.push(init[i]);
+    }
+    
+    for (let j = 0; j < pos.length; j++) {
+      init.splice(j, 1, movedCycleArray[pos[j]]);
+    }
+  }
+
+  function rotateTop(line, face, dir) {
+    const movingLineArray = selectMovingFace(line, face);
 
     if (dir === CUBE_DIR.LR) {
       movingCellArray = movingLineArray.shift();
@@ -148,14 +231,7 @@
   }
 
   function rotateBottom(line, face, dir) {
-    console.log(line, face, dir);
-    for (let i = 0; i < initCubeArray.length; i++) {
-      faceArray.push(initCubeArray[i]);
-    }
-
-    for (let i = 0; i < line.length; i++) {
-      movingLineArray.push(initCubeArray[line[i]].splice(divisionNum * 2));
-    }
+    const movingLineArray = selectMovingFace(line, face);
 
     if (dir === CUBE_DIR.RL) {
       movingCellArray = movingLineArray.shift();
@@ -173,40 +249,30 @@
     rotateFaceOfCube(initCubeArray[face], rotatePosition(dir));
   }
 
-  function rotateLine(line) {
-    const selectedLine = {
-      0: [1, 2, 3, 4],
-      1: [0, 2, 5, 4],
-      2: [0, 1, 3, 5],
-      3: [0, 2, 5, 4],
-      4: [0, 1, 2, 3],
-      5: [1, 2, 3, 4],
+  function rotateLeft(line, face, dir, lnr) {
+    const movingLineArray = selectMovingFace(line, face);
+    const copyMovingLineArray = JSON.parse(JSON.stringify(movingLineArray));
+    const sideCellIndex = rotateSideCell(lnr);
+    const num = 2;
+
+    movingCellArray = copyMovingLineArray.pop();
+    copyMovingLineArray.unshift(movingCellArray);
+
+    for (let i = 0; i < line.length; i++) {
+      for (let j = 0; j < sideCellIndex.length; j++) {
+        if (i === 0) {
+          initCubeArray[line[i]][sideCellIndex[j]] = copyMovingLineArray[i][sideCellIndex[j] + num];
+        } else if (i === line.length - 1) {
+          initCubeArray[line[i]][sideCellIndex[j] + num] = copyMovingLineArray[i][sideCellIndex[j]];
+        } else {
+          initCubeArray[line[i]][sideCellIndex[j]] = copyMovingLineArray[i][sideCellIndex[j]];
+        }
+      }
     }
 
-    return selectedLine[line];
+    rotateFaceOfCube(initCubeArray[face], rotatePosition(dir));
   }
-
-  function rotatePosition(line) {
-    const selectedLine = {
-      "from L to R": [6, 3, 0, 7, 4, 1, 8, 5, 2],
-      "from R to L": [2, 5, 8, 1, 4, 7, 0, 3, 6],
-    }
-
-    return selectedLine[line];
-  }
-
-  function rotateFaceOfCube(init, pos) {
-    const movedCycleArray = [];
-
-    for (let i = 0; i < init.length; i++) {
-      movedCycleArray.push(init[i]);
-    }
-    
-    for (let j = 0; j < pos.length; j++) {
-      init.splice(j, 1, movedCycleArray[pos[j]]);
-    }
-  }
-
+  
   function extendFlatArray(nestedArray) {
     var result = [];
 
