0dd0f019a98aff7c78f83378643982d7f211d22f
mook <mook9288@gmail.com>
2021-04-02 17:54:01 +0900

    Add an equal button click event

--- vanillaJS/calculator/calculator.js
+++ vanillaJS/calculator/calculator.js
@@ -4,6 +4,7 @@ const btnNumber = document.querySelectorAll(".number");
 const btnOperation = document.querySelectorAll(".operation");
 const btnReset = document.querySelector(".reset");
 const btnCancle = document.querySelector(".cancle");
+const btnEquals = document.querySelector(".equals");
 
 [...button].forEach((element) => {
   element.dataset.btnValue = element.innerText;
@@ -17,7 +18,9 @@ const btnCancle = document.querySelector(".cancle");
 
 [...btnOperation].forEach((element) => {
   element.addEventListener("click", (e) => {
-    if (!/[+-/*]$/.test(result.value)) {
+    if(!result.value) {
+      result.value = '';
+    } else if (!/[+-/*]$/.test(result.value)) {
       result.value += element.dataset.btnValue;
     } else {
       result.value = result.value.replace(/[+-/*]$/, element.dataset.btnValue);
@@ -32,3 +35,23 @@ btnReset.addEventListener("click", () => {
 btnCancle.addEventListener("click", () => {
   result.value = result.value.slice(0, -1);
 });
+
+let timer = '';
+btnEquals.addEventListener("click", () => {
+  if (!/[+-/*]$/.test(result.value)) {
+    return result.value = eval(result.value);
+  } else {
+    if (!document.querySelector(".calculator--alert")) {
+      return reportIncomplete();
+    }
+  }
+});
+
+const reportIncomplete = function() {
+  const layout = '<div class="calculator--alert">완성되지 않은 수식입니다.</div>';
+  document.querySelector(".calculator").insertAdjacentHTML("beforeend", layout);
+  timer = setInterval(() => {
+    clearInterval(timer);
+    document.querySelector(".calculator--alert").remove();
+  }, 3000);
+}
